a54d1112c20e5db3f659525b1f6a213f9815d12c
Peter Hizalev <peter.hizalev@gmail.com>
2022-05-06 16:17:39 -0700

    Compiler argument for input shapes

--- tools/src/tensil/tools/CompilerInputShapes.scala
+++ tools/src/tensil/tools/CompilerInputShapes.scala
@@ -1,6 +1,37 @@
 package tensil.tools
 
 object CompilerInputShapes {
+  def parse(shapeStrings: String): CompilerInputShapes = {
+    shapeStrings
+      .split("]\\S*,")
+      .map(_.trim())
+      .map(shapeString => {
+        val Seq(nameString, dimsString) =
+          shapeString.split('[').map(_.trim()).toSeq
+
+        val shape = dimsString
+          .split(']')(0)
+          .split(',')
+          .map(_.trim())
+          .map(dimString =>
+            if (dimString.isEmpty())
+              None
+            else
+              Some(Integer.parseInt(dimString))
+          )
+          .toSeq
+
+        val name =
+          if (nameString.isEmpty())
+            None
+          else
+            Some(nameString)
+
+        (name, shape)
+      })
+      .toMap
+  }
+
   def mkWithBatchSize(batchSize: Int): CompilerInputShapes =
     Map(
       None -> Seq(Some(batchSize))

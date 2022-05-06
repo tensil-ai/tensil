60e76585eb3241df917f888ab29843abee7c2d6c
Tom Alcorn <tdb.alcorn@gmail.com>
2022-05-06 11:49:51 -0700

    Implement write safety logic

--- rtl/src/test/scala/tensil/Scratch.scala
+++ rtl/src/test/scala/tensil/Scratch.scala
@@ -10,6 +10,7 @@ import chisel3.util.Decoupled
 import chiseltest._
 import tensil.util.decoupled.Counter
 import chisel3.util.Queue
+import chisel3.util.DecoupledIO
 
 class Scratch extends Module {
   val gen               = UInt(8.W)
@@ -21,12 +22,12 @@ class Scratch extends Module {
   val io = IO(new Bundle {
 
     val a = new Bundle {
-      val in  = new Port(gen, depth)
-      val out = Flipped(new Port(gen, depth))
+      val in  = Flipped(Decoupled(new Request(depth)))
+      val out = Decoupled(new Request(depth))
     }
     val b = new Bundle {
-      val in  = new Port(gen, depth)
-      val out = Flipped(new Port(gen, depth))
+      val in  = Flipped(Decoupled(new Request(depth)))
+      val out = Decoupled(new Request(depth))
     }
   })
 
@@ -38,19 +39,57 @@ class Scratch extends Module {
   val a = io.a
   val b = io.b
 
-  val aControlQueue = Queue(a.in.control, controlBufferSize)
-  val bControlQueue = Queue(b.in.control, controlBufferSize)
+  val aQueue = Queue(a.in, controlBufferSize)
+  val bQueue = Queue(b.in, controlBufferSize)
 
-  aTable.io.incr.address := a.in.control.bits.address
-  aTable.io.incr.enable := a.in.control.bits.write
-  aTable.io.decr.address := aControlQueue.bits.address
-  aTable.io.decr.enable := aControlQueue.bits.write
+  val bSafe = connectTable(aTable, a.in, aQueue, bQueue)
+  val aSafe = connectTable(bTable, b.in, bQueue, aQueue)
 
-  bTable.io.incr.address := b.in.control.bits.address
-  bTable.io.incr.enable := b.in.control.bits.write
-  bTable.io.decr.address := bControlQueue.bits.address
-  bTable.io.decr.enable := bControlQueue.bits.write
+  when(aSafe) {
+    when(bSafe) {
+      // both can proceed
+      a.out <> aQueue
+      b.out <> bQueue
+    }.otherwise {
+      // a can proceed, b cannot
+      a.out <> aQueue
+      bQueue.ready := false.B
+      b.out.valid := false.B
+      b.out.bits <> bQueue.bits
+    }
+  }.otherwise {
+    when(bSafe) {
+      // b can proceed, a cannot
+      aQueue.ready := false.B
+      a.out.valid := false.B
+      a.out.bits <> aQueue.bits
+      b.out <> bQueue
+    }.otherwise {
+      // both unsafe to proceed, a gets priority to ensure progress is made
+      a.out <> aQueue
+      bQueue.ready := false.B
+      b.out.valid := false.B
+      b.out.bits <> bQueue.bits
+    }
+  }
 
+  def connectTable(
+      table: CounterTable,
+      in: DecoupledIO[Request],
+      queue: DecoupledIO[Request],
+      altQueue: DecoupledIO[Request],
+  ): Bool = {
+    table.io.incr.address := in.bits.address
+    table.io.incr.enable := in.fire() && in.bits.write
+    table.io.decr.address := queue.bits.address
+    table.io.decr.enable := queue.fire() && queue.bits.write
+
+    table.io.read.address := altQueue.bits.address
+    table.io.read.enable := altQueue.valid
+    // safe for the alternate control queue to proceed
+    val altSafe = !(table.io.read.enable && table.io.count =/= 0.U)
+    altSafe
+  }
 }
 
 class CounterTablePort(val size: Int) extends Bundle {
@@ -60,18 +99,18 @@ class CounterTablePort(val size: Int) extends Bundle {
 
 class CounterTable(size: Int, maxCount: Int) extends Module {
   val io = IO(new Bundle {
-    val incr = Input(new CounterTablePort(size))
-    val decr = Input(new CounterTablePort(size))
-    val read = Input(new CounterTablePort(size))
-    val data = Output(UInt(log2Ceil(size).W))
+    val incr  = Input(new CounterTablePort(size))
+    val decr  = Input(new CounterTablePort(size))
+    val read  = Input(new CounterTablePort(size))
+    val count = Output(UInt(log2Ceil(size).W))
   })
 
   val table = RegInit(VecInit(Array.fill(size)(0.U(log2Ceil(maxCount).W))))
 
   when(io.read.enable) {
-    io.data := table(io.read.address)
+    io.count := table(io.read.address)
   }.otherwise {
-    io.data := DontCare
+    io.count := DontCare
   }
 
   when(io.incr.address === io.decr.address) {
@@ -98,7 +137,7 @@ class CounterTable(size: Int, maxCount: Int) extends Module {
     }
   }
 
-  def incr(): Unit = {
+  private def incr(): Unit = {
     table(io.incr.address) := Mux(
       table(io.incr.address) === (maxCount - 1).U,
       table(io.incr.address),
@@ -106,7 +145,7 @@ class CounterTable(size: Int, maxCount: Int) extends Module {
     )
   }
 
-  def decr(): Unit = {
+  private def decr(): Unit = {
     table(io.decr.address) := Mux(
       table(io.decr.address) === 0.U,
       table(io.decr.address),
@@ -147,7 +186,15 @@ class ScratchSpec extends UnitSpec {
   behavior of "Scratch"
 
   it should "work" in {
-    test(new Scratch) { m => }
+    test(new Scratch) { m =>
+      m.io.a.in.setSourceClock(m.clock)
+      m.io.a.out.setSinkClock(m.clock)
+      m.io.b.in.setSourceClock(m.clock)
+      m.io.b.out.setSinkClock(m.clock)
+
+      m.io.a.in.enqueue(Request(m.depth, 0, true))
+      m.io.b.in.enqueue(Request(m.depth, 0, false))
+    }
   }
 }
 
@@ -163,4 +210,10 @@ class Port[T <: Data](gen: T, depth: Long) extends Bundle {
 class Request(val depth: Long) extends Bundle {
   val address = UInt(log2Ceil(depth).W)
   val write   = Bool()
+}
+
+object Request {
+  def apply(depth: Long, address: Int, write: Boolean): Request = {
+    (new Request(depth)).Lit(_.address -> address.U, _.write -> write.B)
+  }
 }
